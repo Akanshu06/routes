2c000db2931424660461255d25a0067b3325f2ed
Akanshu <122857774+Akanshu06@users.noreply.github.com>
2024-03-04 10:35:34 +0530

    usingRouter

---
+++ routes/shop.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+router.get('/', (req, res, next) => {
+    res.send('<h2>Hello from Express</h2>');
+});
+
+module.exports=router;
